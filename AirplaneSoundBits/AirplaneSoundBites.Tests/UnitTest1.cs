cc18edd37d1c21ba918ce95a68099dc3e818272e
Kyle Kornetzke <kyle.kornetzke@gmail.com>
2016-03-02 21:13:40 -0600

    Adding application to play sound bites from the move Airplane.

---
+++ AirplaneSoundBits/AirplaneSoundBites.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AirplaneSoundBites.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirplaneSoundBites.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private AirplaneSoundBites
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AirplaneSoundBits/AirplaneSoundBits.Service/AirplanePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirplaneSoundBits.Service
+{
+    public class AirplanePlayer : SoundPlayer
+    {
+        public string FolderPath { get; internal set; }
+
+        public string[] AudioTitles { get { return GetTitles(); } }
+
+        private string[] GetTitles()
+        {
+            return Directory.GetFiles(FolderPath, "*.wav");
+        }
+
+        public AirplanePlayer(string folderLocation)
+        {
+            this.FolderPath = folderLocation;
+        }
+        
+    }
+}
